63141cd4c87c0316061f86564436ece10ae59729
bieradek <biedrzycki.radek@gmail.com>
2018-08-27 23:41:13 +0200

    Initial commit

---
+++ src/com/timbuchalka/Main.java
@@ -0,0 +1,23 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        create a base class called car
+        it should have few fields that would be best for a generic car class like engine, cylinders, wheels etc.
+        constructor should initialize cylinders(number of) and name, and set wheels to 4 and engine to true
+        cylinders and names would be bpassed parameters
+        //
+        create getters
+        //
+        create some methods like start engine, accelerate and brake
+        //
+        show a message for each in the base class
+        now create 3 sub classes for your favorite vehicles
+        override the appropiate methods to demonstrate polymorphism in use
+        //
+        put all classes in one java file
+         */
+    }
+}
