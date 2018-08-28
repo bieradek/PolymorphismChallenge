045a886f9d928046797b0dafca47bed23b2e49e8
bieradek <biedrzycki.radek@gmail.com>
2018-08-28 23:26:04 +0200

    Uploading final code and introducing getClass() and getSimpleName()

--- src/TimsCode/Main.java
+++ src/TimsCode/Main.java
@@ -34,6 +34,69 @@ class Car {
     }
 }
 
+class Mitsubishi extends Car {
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Mitsubishi --> start engine";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Mitsubishi --> accelerate";
+    }
+
+    @Override
+    public String brake() {
+        return "Mitsubishi --> brake";
+    }
+}
+
+class Ford extends Car {
+    public Ford(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Ford --> start engine";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Ford --> accelerate";
+    }
+
+    @Override
+    public String brake() {
+        return "Ford --> brake";
+    }
+}
+
+class Holden extends Car {
+    public Holden(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return getClass().getSimpleName() + " start engine";
+    }
+
+    @Override
+    public String accelerate() {
+        return getClass().getSimpleName() + " accelerate";
+    }
+
+    @Override
+    public String brake() {
+        return getClass().getSimpleName() + " brake";
+    }
+}
+
 
 public class Main {
     public static void main(String[] args) {
@@ -53,5 +116,24 @@ public class Main {
         //
         put all classes in one java file
          */
+        Car car = new Car(8, "Base car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRW 4WD");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+
+        Ford ford = new Ford(6, "Ford Falcon");
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+
+        Holden holden = new Holden(6, "Holden Commodore");
+        System.out.println(holden.startEngine());
+        System.out.println(holden.accelerate());
+        System.out.println(holden.brake());
     }
 }
